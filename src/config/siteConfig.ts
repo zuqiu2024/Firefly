3f866f043606cdb0b51c4a2306374350c8dc0bc5
zuqiu2024 <zhanxuxiang2022@163.com>
2026-03-28 20:07:35 +0800

    更新 siteConfig.ts

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -13,11 +13,11 @@ export const siteConfig: SiteConfig = {
 	subtitle: "Go Go Go",
 
 	// 站点 URL
-	site_url: "https://firefly.cuteleaf.cn",
+	site_url: "https://zhanxx81201.dpdns.org",
 
 	// 站点描述
 	description:
-		"Firefly 是一款基于 Astro 框架和 Fuwari 模板开发的清新美观且现代化个人博客主题模板，专为技术爱好者和内容创作者设计。该主题融合了现代 Web 技术栈，提供了丰富的功能模块和高度可定制的界面，让您能够轻松打造出专业且美观的个人博客网站。",
+		"此为基于 Firefly改编",
 
 	// 站点关键词
 	keywords: [
