a7c4178b5dc4d8d617c9bfbd989d88aebde2b71f
zuqiu2024 <zhanxuxiang2022@163.com>
2026-04-11 14:32:05 +0800

    更新 sponsorConfig.ts

--- src/config/sponsorConfig.ts
+++ src/config/sponsorConfig.ts
@@ -31,7 +31,7 @@ export const sponsorConfig: SponsorConfig = {
 		{
 			name: "微信",
 			icon: "fa7-brands:weixin",
-			qrCode: "/assets/images/sponsor/wechat.png",
+			qrCode: "/assets/images/sponsor/wechat.jpg",
 			link: "",
 			description: "使用 微信 扫码赞助",
 			enabled: true,
