8a413a111de3d542550302bf45ce7b5372dce0c9
zuqiu2024 <zhanxuxiang2022@163.com>
2026-04-11 10:30:54 +0800

    更新 profileConfig.ts

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -6,7 +6,7 @@ export const profileConfig: ProfileConfig = {
 	// 1. public 目录（以 "/" 开头，不优化）："/assets/images/avatar.webp"
 	// 2. src 目录（不以 "/" 开头，自动优化但会增加构建时间，推荐）："assets/images/avatar.webp"
 	// 3. 远程 URL："https://example.com/avatar.jpg"
-	url: "src/assets/images/avatar.jpg",
+	avatar: "assets/images/avatar.avif",
 
 	// 名字
 	name: "Firefly",
