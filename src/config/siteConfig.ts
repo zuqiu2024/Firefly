ca5a24ab4703a185b0b62891941bbf623237f770
zuqiu2024 <zhanxuxiang2022@163.com>
2026-04-17 22:34:55 +0800

    更新 siteConfig.ts

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -89,7 +89,7 @@ export const siteConfig: SiteConfig = {
 	},
 
 	// 站点开始日期，用于统计运行天数
-	siteStartDate: "2025-01-01",
+	siteStartDate: "2026-01-01",
 
 	// 站点时区（IANA 时区字符串），用于格式化bangumi、rss里的构建日期时间等等..
 	// 示例："Asia/Shanghai", "UTC", 如果为空，则按照构建服务器的时区进行时区转换
