15ed0dad15209ccf0e58259d9c6c3bd2bbd689bd
zuqiu2024 <zhanxuxiang2022@163.com>
2026-04-25 03:49:05 +0800

    更新 commentConfig.ts

--- src/config/commentConfig.ts
+++ src/config/commentConfig.ts
@@ -6,7 +6,7 @@ export const commentConfig: CommentConfig = {
 
 	//twikoo评论系统配置
 	twikoo: {
-		envId: "https://pinglun-sigma-seven.vercel.app",
+		envId: "https://zhanxx0301.dpdns.org",
 		// 设置 Twikoo 评论系统语言
 		lang: "zh-CN",
 		// 是否启用文章访问量统计功能
